d8eb1902dfb42487554ce05dc54d8c3c223d6530
Mikey Badr <mfbadr@gmail.com>
2014-08-05 10:30:47 -0500

    refactoring

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -4,11 +4,13 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var home = require('../controllers/home');
 var items = require('../controllers/items');
+var methodOverride = require('express-method-override');
 
 module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride()); 
 
   app.get('/', home.index);
   app.get('/faq', home.faq);
@@ -18,7 +20,7 @@ module.exports = function(app, express){
   app.post('/items', items.create);
   app.get('/items', items.index);
   app.get('/items/:id', items.show);
-  app.post('/items/:id', items.destroy);
+  app.delete('/items/:id', items.destroy);
 
   console.log('pipeline is GO!');
 };
