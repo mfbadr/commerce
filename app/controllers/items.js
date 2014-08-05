b50507eb9ee2d6d83e60d32a8d979d07860910dd
Mikey Badr <mfbadr@gmail.com>
2014-08-05 09:14:24 -0500

    final

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -16,7 +16,7 @@ exports.create = function(req, res){
   console.log(req.body);
   var item = new Item(req.body);
   item.save(function(){
-    res.redirect('/');
+    res.redirect('/items');
   });
 };
 
